2c0195fc301584038ec0fb18e41ebfe6a4063be6
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-02 13:24:08 +0530

    Create reverse_of_an_array.cpp

---
+++ array/reverse_of_an_array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Enter the size of your array: ";
+	cin>> n;
+	int a[n];
+	int b[n];
+	cout << "input your "<<n <<" numbers: "<<endl;
+	for (int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	cout << "Reverese of your array is: ";
+	for (int i=n-1;i>=0;i--)
+	{
+		cout <<  a[i]<< " ";
+	}
+	return 0;
+}
