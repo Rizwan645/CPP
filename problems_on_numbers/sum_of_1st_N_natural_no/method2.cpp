20114c678a6df269f6d094865416171ef02ad055
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-03 17:16:08 +0530

    Create method2.cpp

---
+++ problems_on_numbers/sum_of_1st_N_natural_no/method2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	int sum=0;
+	string x;
+    cout << "~~SUM OF 1ST N NATURAL NUMBERS~~"<<endl;
+    cout << " input the value of N: ";
+    cin >> n;
+   	cout << "sum="<<((n*(n+1))/2);
+    return 0;
+}
