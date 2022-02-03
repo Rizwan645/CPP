bae39205a3e11c4de4ac31a18d16fc0e39726857
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-03 17:02:21 +0530

    Create positive_or_negative_checking.cpp

---
+++ problems_on_numbers/positive_or_negative_checking.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	string x;
+    cout << "Input a Number: ";
+    cin >> n;
+   	if(n>0)
+   	{
+   		cout<<n<<" is a positive no";
+	}
+	else
+	{
+		cout<<n<<" is a positive no";
+	}
+    return 0;
+}
