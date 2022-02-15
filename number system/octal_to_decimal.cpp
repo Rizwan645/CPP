c40cad638bc154a74f3355bd616dc1cb8bec93fe
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-15 15:10:40 +0530

    Update octal_to_decimal.cpp

--- number system/octal_to_decimal.cpp
+++ number system/octal_to_decimal.cpp
@@ -7,15 +7,15 @@ int main()
 	int n;
 	int i=0;
 	int s=0;
-    cout << "Enter the decimal no: ";
+    cout << "Enter the octal no: ";
     cin>>n;
     while(n>0)
     {
-    	s=s+((n%8)*pow(10,i));
-    	n=n/8;
+    	s=s+((n%10)*pow(8,i));
+    	n=n/10;
     	i++;
 	}
-	cout << "your number in octal: "<<s;
+	cout << "your number in decimal: "<<s;
 	return 0;
     
 }
