7f3091df08abc401cd2feb8f989fbee5286ae1cb
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-20 23:55:30 +0530

    Create Roots_of_Quadratic_Equation.cpp

---
+++ problems_on_numbers/Roots_of_Quadratic_Equation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    int d;
+    float r1,r2;
+    double r11,r22;
+    cout << "The general form of a Quadratic equation: ax^2 + bx + c"<<endl;
+    cout<<"Enter the value of a : ";
+    cin >>a;
+    cout<<"Enter the value of b : ";
+    cin >>b;
+    cout<<"Enter the value of c : ";
+    cin >>c;
+    d=(b*b)-(4*a*c);
+    if(d>=0)
+    {
+        r1=(((-1)*b)+sqrt(d))/(2*a);
+        r2=(((-1)*b)-sqrt(d))/(2*a);
+        cout<<"Roots: "<<r1<<" , "<<r2;
+    }
+    else
+    {
+        r1=((-1)*b)/(2*a);
+        r11= (sqrt(-d))/(2*a);
+        r22=(sqrt(-d))/(2*a);
+        cout<<"Roots: "<<r1<<" + i"<<r11<<" , "<<r1<<"-i"<<r22;
+    }
+    
+    return 0;
+}
