fb4a69abd4b947a81facbc2fe89f1d01d69d6dcc
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-20 23:07:12 +0530

    Create Area_of_a_circle.cpp

---
+++ problems_on_numbers/Area_of_a_circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r;
+    double area;
+    cout<<"Enter the value of Radius : ";
+    cin >>r;
+    area=3.14*r*r;
+    cout<<"Area of the circle is: "<<area;
+    return 0;
+}
