26386590212ac9d7a48564b375eb614d5a97e894
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-02-20 20:21:06 +0530

    Create GCD_of_2_Numbers.cpp

---
+++ problems_on_numbers/GCD_of_2_Numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    int min=0;
+    cout<<"Enter 2 numbers : "<<endl;
+    cin >>a>>b;
+    if(a<b)
+    {
+        min=a;
+    }
+    else
+    {
+        min=b;
+    }
+    for(int i=1;i<=min;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            c=i;
+        }
+    }
+    cout << "GCD of "<<a<<" and "<<b<<" = "<<c;
+    return 0;
+}
