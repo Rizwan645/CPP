fb68a4da1af182caaeef90385ed2da6e35a3e5ad
SK MD RIZWAN <97179885+Rizwan645@users.noreply.github.com>
2022-03-11 15:23:53 +0530

    Create all_prime_numbers_in_a_range.cpp

---
+++ problems_on_numbers/all_prime_numbers_in_a_range.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int start, end;
+    cout<< "Enter the lower limit:";
+    cin>> start;
+    cout<< "Enter the upper limit:";
+    cin>> end;
+    cout<< "Prime numbers between "<< start << " and " << end << " are: ";
+    int n=start;
+    int c=0;
+    while (n<=end)
+    {
+        for(int i=2;i<=n-1;i++)
+        {
+            if(n%i==0)
+            {
+                c++;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            cout<<n<<" ";
+        }
+        c=0;
+        n++;
+    }
+   return 0;
+}
